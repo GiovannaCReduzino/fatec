63dffe9f1150a5d66a0e1d64f65d1b286ee7b308
GiovannaCReduzino <gigi.reduzino@gmail.com>
2024-08-22 22:32:14 -0300

    Assuntos tratados em aula

---
+++ TecDeProgramacao/ControleCondicional.py
@@ -0,0 +1,11 @@
+'''
+Uma condição é uma expressão que resulte numa resposta do tipo 
+verdadeiro ou falso.
+
+Por exemplo, para a condição 𝑥 > 0 temos que:
+    Se o valor de x for um valor POSITIVO, a condição será considerada verdadeira;
+
+    Se o valor de x igual a ZERO ou NEGATIVO, a condição será considerada falsa;
+
+Uma condição pode ser construída utilizando operadores matemáticos, relacionais e lógicos.
+'''
---
+++ TecDeProgramacao/PrecedenciaOperadores.py
@@ -0,0 +1,26 @@
+'''
+A precedência de operadores em Python determina a ordem em que os operadores 
+são avaliados em uma expressão. Quando uma expressão contém vários 
+operadores, a precedência define qual operador será avaliado primeiro, qual será o 
+próximo e assim por diante.
+'''
+
+# Maior precedência > Menor precedência
+
+'''
+() Dentro dos parênteses é sempre avaliado primeiro;
+[ ] Elemento de lista, acesso a índice;
+. Elemento de objeto, acesso a atributo;
+** Potência;
++x, -x Adição e subtração unária
+*, /, % Multiplicação, divisão e resto
++, - Adição e subtração
+<<, >> Deslocamento de bits à esquerda e à direita
+^ OU exclusivo
+in, not in, is, is not, 
+<, <=, >, >=, ==, != Operadores de comparação
+not x NÃO lógico
+and E lógico
+or OU lógico
+= Atribuição
+'''
